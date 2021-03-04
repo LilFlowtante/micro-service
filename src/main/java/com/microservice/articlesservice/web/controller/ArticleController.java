b51a7cf42a72e6cf0a1e461410c6f45d5f1880b2
FRAIX Hugo - m115263 <hugo.fraix@soprasteria.com>
2021-03-04 10:13:13 +0100

    Add request id logger to request mapping

--- src/main/java/com/microservice/articlesservice/web/controller/ArticleController.java
+++ src/main/java/com/microservice/articlesservice/web/controller/ArticleController.java
@@ -4,6 +4,10 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJacksonValue;
@@ -31,12 +35,19 @@ import io.swagger.annotations.ApiOperation;
 @Api(description = "API pour les opérations CRUD sur les articles.")
 public class ArticleController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(ArticleController.class);
+	
+	@Autowired
+	private HttpServletRequest requestContext;
+	
 	@Autowired
 	private ArticleDao articleDao;
 	
 	@ApiOperation(value = "Récupère la liste de tous les articles.")
 	@GetMapping(value = "/Articles")
 	public MappingJacksonValue listeArticles() {
+		logger.info("Début d'appel au service Articles pour la requête: " + requestContext.getHeader("req-id"));
+		
 		List<Article> articles = articleDao.findAll();
 		
 		SimpleBeanPropertyFilter monFiltre = SimpleBeanPropertyFilter.serializeAllExcept("prixAchat");
@@ -53,6 +64,8 @@ public class ArticleController {
 	@ApiOperation(value = "Récupère un article grâce à son ID à condition que celui-ci soit en stock!")
 	@GetMapping(value = "/Articles/{id}")
 	public Article afficherUnArticle(@PathVariable int id) {
+		logger.info("Début d'appel au service Articles pour la requête: " + requestContext.getHeader("req-id"));
+		
 		Article article = articleDao.findById(id);
 		if(article == null) {
 			throw new ArticleIntrouvableException("L'article avec l'id " + id + " est INTROUVABLE.");
@@ -63,6 +76,8 @@ public class ArticleController {
 	@ApiOperation(value = "Calcule la marge de chaque article (différence entre prix d'achat et prix de vente).")
 	@GetMapping(value = "/AdminArticles")
 	public List<String> calculerMargeArticle() {
+		logger.info("Début d'appel au service Articles pour la requête: " + requestContext.getHeader("req-id"));
+		
 		List<Article> articles = articleDao.findAll();
 		List<String> resultArray = new ArrayList<>();
 		for(Article article: articles) {
@@ -76,17 +91,23 @@ public class ArticleController {
 	@ApiOperation(value = "Retourne la liste de tous les articles triés par nom croissant.")
 	@GetMapping(value = "/Articles/order")
 	public List<Article> trierArticlesParOrdreAlphabetique() {
+		logger.info("Début d'appel au service Articles pour la requête: " + requestContext.getHeader("req-id"));
+		
 		return articleDao.findAllByOrderByNomAsc();
 	}
 	
 	/* TEST MAPPING - DEBUT */
 	@GetMapping(value = "/test/articles/{prixLimit}")
 	public List<Article> testePrixLimite(@PathVariable int prixLimit) {
+		logger.info("Début d'appel au service Articles pour la requête: " + requestContext.getHeader("req-id"));
+		
 		return articleDao.findByPrixGreaterThan(prixLimit);
 	}
 	
 	@GetMapping(value = "/test/articles/like/{recherche}")
 	public List<Article> testeLike(@PathVariable String recherche) {
+		logger.info("Début d'appel au service Articles pour la requête: " + requestContext.getHeader("req-id"));
+		
 		return articleDao.findByNomLike("%"+recherche+"%");
 	}
 	/* TEST MAPPING - FIN */
@@ -94,6 +115,7 @@ public class ArticleController {
 	@ApiOperation(value = "Ajoute un nouvel article.")
 	@PostMapping(value = "/Articles")
 	public ResponseEntity<Void> ajouterArticle(@RequestBody Article article) {
+		logger.info("Début d'appel au service Articles pour la requête: " + requestContext.getHeader("req-id"));
 		
 		if(article.getPrix() == 0) {
 			throw new ArticlePrixEgalZeroException("Impossible d'enregistrer un article dont le prix est 0.");
@@ -117,12 +139,16 @@ public class ArticleController {
 	@ApiOperation(value = "Supprime l'article par son ID.")
 	@DeleteMapping(value = "/Articles/{id}")
 	public void supprimerArticle(@PathVariable int id) {
+		logger.info("Début d'appel au service Articles pour la requête: " + requestContext.getHeader("req-id"));
+		
 		articleDao.deleteById(id);
 	}
 	
 	@ApiOperation(value = "Met à jour l'article.")
 	@PutMapping(value = "/Articles")
 	public void updateArticle(@RequestBody Article article) {
+		logger.info("Début d'appel au service Articles pour la requête: " + requestContext.getHeader("req-id"));
+		
 		articleDao.save(article);
 	}
 }
